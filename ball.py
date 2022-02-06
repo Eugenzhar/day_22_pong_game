bf986d203e197d0e614cfa3eeea1cf7a5ccfecae
Eugene <demontage09@gmail.com>
2022-02-07 01:09:42 +0500

    start pong

---
+++ ball.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.penup()
+        self.mov_x = 10
+        self.mov_y = 10
+
+
+    def move(self):
+            new_x = self.xcor() + self.mov_x
+            new_y = self.ycor() + self.mov_y
+            self.goto(new_x, new_y)
+
+    def bounce(self):
+        self.mov_y *= -1
+
+    def bounce_x(self):
+        self.mov_x *= -1
+
---
+++ main.py
@@ -0,0 +1,39 @@
+import time
+from turtle import Turtle, Screen
+from paddle import Paddle
+from ball import Ball
+
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("My Pong")
+screen.tracer(0)
+
+
+paddle_r = Paddle((350, 0))
+paddle_l = Paddle((-350, 0))
+ball = Ball()
+
+
+screen.listen()
+screen.onkey(paddle_r.go_up, "Up")
+screen.onkey(paddle_r.go_down, "Down")
+screen.onkey(paddle_l.go_up, "w")
+screen.onkey(paddle_l.go_down, "s")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
+
+    if ball.distance(paddle_l) < 25 or ball.distance(paddle_r) < 25:
+        ball.bounce_x()
+
+
+screen.exitonclick()
